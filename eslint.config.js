38a33a01080849b1047a420980145bd9a9124b63
Jeff K <jeffery.keene@gsa.gov>
2026-06-10 16:26:24 -0400

    added eslint config and fixed dependencies

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import svelte from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".svelte"],
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      svelte,
+    },
+    rules: {
+      ...svelte.configs.recommended.rules,
+      "svelte/no-unused-svelte-ignore": "warn",
+      "svelte/valid-compile": "error",
+    },
+  },
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      ".svelte-kit/**",
+      "build/**",
+      "storybook-static/**",
+      "coverage/**",
+      "*.config.js",
+      "*.config.ts",
+    ],
+  },
+  prettier,
+];
--- src/lib/ambient.d.ts
+++ src/lib/ambient.d.ts
@@ -5,6 +5,7 @@
 
 declare module "*.svelte" {
   import type { Component } from "svelte";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const component: Component<any>;
   export default component;
   // Allow type exports from Svelte components
